a902aca2a199b0aa4573b96fa8f9b8eddf9b38a8
SkyEagle-100 <gordanov183@gmail.com>
2021-01-22 01:32:51 +0600

    day 2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,22 @@
 package day2;
+import java.util.Scanner;
+
 
 public class Task1 {
     public static void main(String[] args) {
-
+     Scanner sc = new Scanner(System.in);
+     int a = sc.nextInt();
+     if(a>0&&a<=4){
+         System.out.println("Дом малоэтажный");
+     }
+     else if(a>4&&a<9){
+         System.out.println("Дом среднеэтажный");
+     }
+     else if(a<1){
+         System.out.println("Ошибка ввода");
+     }
+     else{
+         System.out.println("Дом многоэтажный");
+     }
     }
 }
--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,19 @@
 package day2;
+import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      for(int i = a+1;i<b;i++){
+          if(a>b){
+              System.out.println("Некорректный ввод");
+              break;
+          }
+          if(i%5==0&&i%10!=0){
+              System.out.println(i);
+          }
+      }
     }
 }
